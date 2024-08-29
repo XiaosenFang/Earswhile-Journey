d1badfb25bf379b2ef595d507af9209c87400427
Xiao_senfang <69411472+XiaosenFang@users.noreply.github.com>
2024-08-29 23:53:57 +0800

    Delete overrides directory

--- overrides/startup_scripts/fluid.js
+++
@@ -1,5 +0,0 @@
-StartupEvents.registry('fluid', event => {
-    event.create('liquid_starlight')
-        .thickTexture(0xfefffe)
-        .bucketColor(0xfefffe)
-})
--- overrides/startup_scripts/infusetype.js
+++
@@ -1,7 +0,0 @@
-const $InfuseTypeDeferredRegister = Java.loadClass("mekanism.common.registration.impl.InfuseTypeDeferredRegister")
-const INFUSETYPE = new $InfuseTypeDeferredRegister("kubejs")
-
-//注册气体 (id, 颜色)
-INFUSETYPE.register("flux_gas", 0x8255e5)
-//所属模组
-INFUSETYPE.register(Java.loadClass("dev.architectury.platform.forge.EventBuses").getModEventBus("kubejs").get())
